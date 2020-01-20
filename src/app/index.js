c6ad7c21bcc2152c9ce2ac9c78e55816aa769730
gurochka <natali-ananyeva@gmail.com>
2020-01-20 12:20:57 +0400

    webpack initial configuration

---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import '../scss/layout.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+console.log('Am I here?')
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.dev.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/app/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js'
+  },
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src/app'),
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },      
+    ]
+  },
+
+  plugins: [
+      // Generates an `index.html` file with the <script> injected.
+      new HtmlWebpackPlugin({
+        template: "./public/index.html"
+      })
+  ],
+
+  devServer: {
+    port: 9000,
+    open: false
+    // openPage: ''
+  }
+};
