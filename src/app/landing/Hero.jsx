35eee88880a2fdf3a59ad6aacda09e7dd673f386
gurochka <natali.ananyeva@gmail.com>
2020-01-31 14:52:10 +0400

    add Activities.jsx section to Landing component

--- src/app/landing/Hero.jsx
+++ src/app/landing/Hero.jsx
@@ -7,7 +7,7 @@ function Hero(){
           <h1 className="mb-3 text-light">A Discworld public library</h1>
           <h2>Explore the magnificent world of fantasy</h2>
           
-          <p>Our adress: <a target="__blank" href="https://www.google.com/maps/place/Ankh-Morpork,+Unseen+University/@53.4867132,18.747349,18z/data=!4m5!3m4!1s0x4702cfddb7134027:0x2d7eebfc6a343fb4!8m2!3d53.486883!4d18.747915"><FontAwesomeIcon icon="place-of-worship"/> Ankh-Morpork, The Unseen University</a></p>
+          <p>Our adress: <a className="tag tag-hover" target="__blank" href="https://www.google.com/maps/place/Ankh-Morpork,+Unseen+University/@53.4867132,18.747349,18z/data=!4m5!3m4!1s0x4702cfddb7134027:0x2d7eebfc6a343fb4!8m2!3d53.486883!4d18.747915"><FontAwesomeIcon icon="place-of-worship"/> Ankh-Morpork, The Unseen University</a></p>
           <div className="flex">
             <div className="mr-3 mt-3">We work at:</div>
             <div className="mt-3 text-w-400 text-gray-300">
--- src/app/landing/Landing.jsx
+++ src/app/landing/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from 'App/landing/Hero.jsx';
+import Activities from 'App/landing/Activities.jsx';
 
 export default class Landing extends React.Component {
   render(){
     return  (
       <div className="landing-page">
         <Hero />
+        <Activities />
         <h1>Landing Component</h1>
       </div>
     );
