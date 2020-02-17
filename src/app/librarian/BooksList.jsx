eca58224e5e67e6b30326940e6e2519cd63403f3
gurochka <natali.ananyeva@gmail.com>
2020-02-17 15:21:31 +0400

    add common BookList component for search and order creation

---
+++ src/app/librarian/BooksList.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import Pagination from 'App/components/Pagination.jsx';
+import BookBriefDefinition from 'App/components/BookBriefDefinition.jsx'
+
+export default class BooksList extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      books: []
+    }
+
+    this.paginate = this.paginate.bind(this)    
+  }
+
+  booksPage = 1
+  totalPages = 0
+
+  serialize(obj){
+    return Object.keys(obj).map(key => {
+      if (Array.isArray(obj[key])) return obj[key].map(val => `${key}=${val}`).join('&')
+        else return `${key}=${obj[key]}`;
+    }).join('&');
+  }
+
+  fetchBooks(){
+    let filters = Object.assign({}, this.props.filters || {}, {
+      _page: this.booksPage,
+      _limit: 10
+    })
+
+    fetch(`http://localhost:3000/books?${this.serialize(filters)}`)
+    .then(res => {
+      let headers = res.headers.get('Link');
+      if (headers) headers = parseInt(headers.match(/_page=(\d)+/g).pop().replace('_page=', ''), 10);
+      this.totalPages = headers || 0;
+      return res.json()      
+    })
+    .then((res,req) => {
+      console.log('books:', res);
+      this.setState({ books: res })
+    })
+  }
+
+  paginate(pageNumber){
+    this.booksPage = pageNumber;
+    this.fetchBooks()
+  }
+
+  componentDidMount() {
+    if (this.props.loadOnStart){
+      this.fetchBooks()
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState,snapshot){
+    if (prevProps.filters != this.props.filters){
+      this.booksPage = 1;
+      this.fetchBooks();
+    }
+  }
+
+  render(){
+    return  (
+      <div className="books mb-5 mr-5">
+        <Pagination page={ this.booksPage } total={this.totalPages} onChange={this.paginate}/>
+
+        { this.state.books.map(book => <BookBriefDefinition book={book} key={book.id}/>)}
+      </div>
+    )
+  }
+}
--- src/app/librarian/OrderNew.jsx
+++ src/app/librarian/OrderNew.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import BooksList from 'App/librarian/BooksList.jsx'
 
 export default class OrderNew extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
 
+    this.state = {
+      content: [],
+      search: ''
     }
+    this.onSearchHandler = this.onSearchHandler.bind(this);
   }
 
-  componentDidMount() {
-
+  onSearchHandler(event){
+    this.setState({ search: event.target.value })
   }
 
   render(){
     return  (
       <div className="page-order-new container my-5">
-        <h1>Order New</h1>
+        <h1>Create New Order</h1>
         <div className="flex">
-          <input type="text" className="form-control flex-grow-1 mb-0 mr-5 py-3" placeholder="Find Book by ISBN, Author or Title"/>
+          <input type="text" className="form-control flex-grow-1 mb-0 mr-5 py-3" placeholder="Find Book by ISBN, Author or Title" 
+            value={this.state.search} 
+            onChange={this.onSearchHandler}/>
           <button className="btn btn-primary btn-xl px-5"><FontAwesomeIcon icon="barcode"/> Or Scan Barcode</button>
+
         </div>
+        
+        <div className="order-content">
+        </div>
+
+        <div className="order-search">
+          <BooksList filters={ { q: this.state.search } } loadOnStart={ false }/>
+        </div>
+
       </div>
     )
   }
